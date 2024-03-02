14153f75d9b07174fe75e8453910295f02ed63f9
Mominur Bashar Ovi <bashar.ovi@outlook.com>
2024-03-02 19:11:33 +0600

    added initial code

---
+++ promise.js
@@ -0,0 +1,29 @@
+function getProduct(id) {
+    let productPromise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if(id > 0){
+          let dummyProduct = {
+            id : 1,
+            name : 'shampoo'
+          };
+          resolve(dummyProduct);
+        }
+        else{
+          let err = new Error('Internal server error');
+          reject(err);
+        }
+      }, 2500);
+    });
+  
+    return productPromise;
+  }
+  
+  let apiRes = getProduct(0);
+  console.log(apiRes);
+  
+  apiRes.then( function(data) {
+    console.log(data)
+  })
+  .catch( function(err) {
+    console.error(err.message);
+  });
