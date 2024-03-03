2453fa88594a0b8462e21d9893bfe61dd4bfe5b8
Mominur Bashar Ovi <bashar.ovi@outlook.com>
2024-03-03 13:57:54 +0600

    added closure hell

---
+++ promise-sequential.js
@@ -0,0 +1,24 @@
+
+
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay)
+    })
+}
+
+
+const executePromise = () => {
+
+delayedColorChange('red', 1000)
+    .then(() => delayedColorChange('green', 1000))
+    .then(() => delayedColorChange('yellow', 1000));
+
+};
+
+
+executePromise();
+
+
