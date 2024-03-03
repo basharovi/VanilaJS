4c87dfced8bf78d567dc56e087519585926e3d2f
Mominur Bashar Ovi <bashar.ovi@outlook.com>
2024-03-03 18:04:42 +0600

    added debugger settings

--- closure.js
+++ closure.js
@@ -1,16 +1,15 @@
 function getProducts() {
-    var products = ["Apple", "Banana", "Orange"];
+  var products = ["Apple", "Banana", "Orange"];
 
-    function getProductById (id) {
-        let product = products[id];
+  function getProductById(id) {
+    let product = products[id];
 
-        if(product)
-            return product;
-        
-        return "Product not found";
-    }
-    
-    return getProductById;
+    if (product) return product;
+
+    return "Product not found";
+  }
+
+  return getProductById;
 }
 
 console.log(getProducts()(5));
