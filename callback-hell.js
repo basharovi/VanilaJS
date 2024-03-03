52f1df7cb4ca5202db2014cbd8416e580fbb7af0
Mominur Bashar Ovi <bashar.ovi@outlook.com>
2024-03-03 18:11:36 +0600

    update indentation

--- callback-hell.js
+++ callback-hell.js
@@ -1,33 +1,30 @@
 const delayedColorChange = (newColor, delay, doNext) => {
-    
-    setTimeout(() => {
-        document.body.style.backgroundColor = newColor;
-        doNext && doNext();
-    }, delay);
+  setTimeout(() => {
+    document.body.style.backgroundColor = newColor;
+    doNext && doNext();
+  }, delay);
 
-    console.log('Next Execution => ', newColor);
-}
+  console.log("Next Execution => ", newColor);
+};
 
 const callbackhell = () => {
+  // hell starts
 
-    // hell starts
-
-    delayedColorChange('red', 1000, () => {
-        delayedColorChange('orange', 1000, () => {
-            delayedColorChange('yellow', 1000, () => {
-                delayedColorChange('green', 1000, () => {
-                    delayedColorChange('blue', 1000, () => {
-                        delayedColorChange('indigo', 1000, () => {
-                            delayedColorChange('violet', 1000, () => {
-                                // hell ends
-                            })
-                        })
-                    })
-                })
-            })
-        })
+  delayedColorChange("red", 1000, () => {
+    delayedColorChange("orange", 1000, () => {
+      delayedColorChange("yellow", 1000, () => {
+        delayedColorChange("green", 1000, () => {
+          delayedColorChange("blue", 1000, () => {
+            delayedColorChange("indigo", 1000, () => {
+              delayedColorChange("violet", 1000, () => {
+                // hell ends
+              });
+            });
+          });
+        });
+      });
     });
-
+  });
 };
 
 callbackhell();
